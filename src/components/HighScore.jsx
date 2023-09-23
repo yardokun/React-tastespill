18842e5c340959938cff5b20402c59e0aed040a4
yardokun <yardenna.okun@gmail.com>
2023-09-23 15:06:41 +0200

    Finished styling

--- src/components/HighScore.jsx
+++ src/components/HighScore.jsx
@@ -1,20 +1,33 @@
+import eikenottImage from "../assets/eikenott.png";
+
 const HighScore = ({ highscores, setRemainingTime }) => {
-    return (
-      <div className="app-wrapper">
-        <div className="center-content">
-          <h1 className="title">Highscoreliste</h1>
-          <ol>
-            {highscores.map((entry, index) => (
-              <li key={index}>
-                {entry.name}: {entry.score}
-              </li>
-            ))}
-          </ol>
-          <button onClick={setRemainingTime}>Spill igjen</button>
-        </div>
+  return (
+    <div className="app-wrapper">
+      <div className="center-content">
+        <h1 className="title">Highscoreliste</h1>
+        <ol>
+          {highscores.map((entry, index) => (
+            <li key={index}>
+              <div className="score-wrapper">
+                <div>
+                  <img
+                    className="eikenott"
+                    src={eikenottImage}
+                    alt="Eikenøtt"
+                  />
+                </div>
+                <div className="style-name">{entry.name}:</div>
+                <div className="style-score">{entry.score}</div>
+              </div>
+            </li>
+          ))}
+        </ol>
+        <button className="start-game-btn" onClick={setRemainingTime}>
+          Spill igjen
+        </button>
       </div>
-    );
-  };
-  
-  export default HighScore;
-  
+    </div>
+  );
+};
+
+export default HighScore;
